5c65537a9532b0eb922158103ed2cd2568592334
inoha-naito <inoha925@gmail.com>
2023-05-14 18:54:44 +0900

    [add] テキストを1文字ずつ表示

--- Assets/Scripts/MainTextController.cs
+++ Assets/Scripts/MainTextController.cs
@@ -8,10 +8,17 @@ namespace NovelGame
     public class MainTextController : MonoBehaviour
     {
         [SerializeField] TextMeshProUGUI _mainTextObject;
+        int _displayedSentenceLength;
+        int _sentenceLength;
+        float _time;
+        float _feedTime;
 
         // Start is called before the first frame update
         void Start()
         {
+            _time = 0f;
+            _feedTime = 0.05f;
+
             string sentence = GameManager.Instance.userScriptManager.GetCurrentSentence();
             if (GameManager.Instance.userScriptManager.IsStatement(sentence))
             {
@@ -24,15 +31,43 @@ namespace NovelGame
         // Update is called once per frame
         void Update()
         {
+            _time += Time.deltaTime;
+            if (_time >= _feedTime)
+            {
+                _time -= _feedTime;
+                if (!CanGoToTheNextLine())
+                {
+                    _displayedSentenceLength++;
+                    _mainTextObject.maxVisibleCharacters = _displayedSentenceLength;
+                }
+            }
+
             if (Input.GetMouseButtonUp(0))
             {
-                GoToTheNextLine();
-                DisplayText();
+                if (CanGoToTheNextLine())
+                {
+                    GoToTheNextLine();
+                    DisplayText();
+                }
+                else
+                {
+                    _displayedSentenceLength = _sentenceLength;
+                }
             }
+        }
+
+        public bool CanGoToTheNextLine()
+        {
+            string sentence = GameManager.Instance.userScriptManager.GetCurrentSentence();
+            _sentenceLength = sentence.Length;
+            return _displayedSentenceLength > sentence.Length;
         }
 
         public void GoToTheNextLine()
         {
+            _displayedSentenceLength = 0;
+            _time = 0f;
+            _mainTextObject.maxVisibleCharacters = 0;
             int count = GameManager.Instance.userScriptManager.GetSentencesCount();
             if (GameManager.Instance.lineNumber < count - 1)
             {
